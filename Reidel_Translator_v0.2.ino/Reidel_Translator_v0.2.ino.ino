026ccb3f0ebbabca6b5624430c3d85780eb36a60
themormansound <59832891+themormansound@users.noreply.github.com>
2022-12-21 13:40:56 +1100

    Add files via upload

---
+++ Reidel_Translator_v0.2.ino/Reidel_Translator_v0.2.ino.ino
@@ -0,0 +1,82 @@
+
+/* This sketch is enumerated as USB MIDI device. 
+ * Following library is required
+ * - MIDI Library by Forty Seven Effects
+ *   https://github.com/FortySevenEffects/arduino_midi_library
+ */
+
+#include <Arduino.h>
+#include <Adafruit_TinyUSB.h>
+#include <MIDI.h>
+
+// USB MIDI object
+Adafruit_USBD_MIDI usb_midi;
+
+int relay = 4;
+
+// Create a new instance of the Arduino MIDI Library,
+// and attach usb_midi as the transport.
+MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MIDI);
+
+void setup()
+{
+#if defined(ARDUINO_ARCH_MBED) && defined(ARDUINO_ARCH_RP2040)
+  // Manual begin() is required on core without built-in support for TinyUSB such as mbed rp2040
+  TinyUSB_Device_Init(0);
+#endif
+
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, HIGH);
+  usb_midi.setStringDescriptor("Riedel-MIDI translator"); 
+
+  // Initialize MIDI, and listen to all MIDI channels
+  // This will also call usb_midi's begin()
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+
+  // Attach the handleNoteOn function to the MIDI Library. It will
+  // be called whenever the Bluefruit receives MIDI Note On messages.
+  MIDI.setHandleNoteOn(handleNoteOn);
+
+  // Do the same for MIDI Note Off messages.
+  MIDI.setHandleNoteOff(handleNoteOff);
+
+  Serial.begin(115200);
+
+  // wait until device mounted
+  while( !TinyUSBDevice.mounted() ) delay(1);
+  digitalWrite(relay, HIGH);
+}
+
+void loop()
+{
+  // read any new MIDI messages
+  MIDI.read();  
+}
+
+void handleNoteOn(byte channel, byte pitch, byte velocity)
+{
+  // Log when a note is pressed.
+  digitalWrite(relay, LOW);
+  Serial.print("Note on: channel = ");
+  Serial.print(channel);
+
+  Serial.print(" pitch = ");
+  Serial.print(pitch);
+
+  Serial.print(" velocity = ");
+  Serial.println(velocity);
+}
+
+void handleNoteOff(byte channel, byte pitch, byte velocity)
+{
+  // Log when a note is released.
+  digitalWrite(relay, HIGH);
+  Serial.print("Note off: channel = ");
+  Serial.print(channel);
+
+  Serial.print(" pitch = ");
+  Serial.print(pitch);
+
+  Serial.print(" velocity = ");
+  Serial.println(velocity);
+}
